9770377997b33b11ecbd53b65d2443968d6ed4af
yasvinippriyaa <139134650+yasvinippriyaa@users.noreply.github.com>
2024-11-11 12:31:11 +0530

    Add files via upload

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,64 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect, createContext } from "react";
+
+import "./App.css";
+import Home from "./components/common/Home";
+import Login from "./components/common/Login";
+import Register from "./components/common/Register";
+import Dashboard from "./components/common/Dashboard";
+import CourseContent from "./components/user/student/CourseContent";
+
+export const UserContext = createContext();
+
+function App() {
+  const date = new Date().getFullYear();
+  const [userData, setUserData] = useState();
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  const getData = async () => {
+    try {
+      const user = await JSON.parse(localStorage.getItem("user"));
+      if (user && user !== undefined) {
+        setUserData(user);
+        setUserLoggedIn(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ userData, userLoggedIn }}>
+      <div className="App">
+        <Router>
+          <div className="content">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              {userLoggedIn ? (
+                <>
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/courseSection/:courseId/:courseTitle" element={<CourseContent />} />
+                </>
+              ) : (
+                <Route path="/login" element={<Login />} />
+              )}
+            </Routes>
+          </div>
+          <footer style={{ backgroundColor: '#003d28', color: '#FFFFFF' }} className="text-center text-lg-start">
+            <div className="text-center p-3">
+              © {date} Copyright: LearnTok
+            </div>
+          </footer>
+        </Router>
+      </div>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
