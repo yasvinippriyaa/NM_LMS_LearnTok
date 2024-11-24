a519b0291bffffc9b62dc7ba7427b262b8c20578
yasvinippriyaa <139134650+yasvinippriyaa@users.noreply.github.com>
2024-11-24 21:19:24 +0530

    Add files via upload

---
+++ src/admin/frontend/src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+
+	return (
+		<div className="container">
+			<h1>LearnTok</h1>
+			<p>
+				<b>Welcome Admin!</b>
+			</p>
+			<p>
+				<b>Goto page of your choice from the navbar. </b>
+			</p>
+		</div>
+	);
+}
+
+export default Home;
